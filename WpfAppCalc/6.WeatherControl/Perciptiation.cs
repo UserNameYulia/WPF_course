047b04e84b2c66d8a686cfa71695db61fa73e591
Yulia Gaifulina <yulia.gaifulina@metran.ru>
2024-09-16 22:37:05 +0500

    add WeatherControl with DependencyProperty Temperature

---
+++ WpfAppCalc/6.WeatherControl/Perciptiation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherControl
+{
+    enum Perciptiation
+    {
+        //0 – солнечно, 1 – облачно, 2 – дождь, 3 – снег.
+        Sunny = 0,
+        Cloudy = 1,
+        Rainy = 2,
+        Snowy = 3,
+    }
+}
---
+++ WpfAppCalc/6.WeatherControl/WeatherControl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WeatherControl
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty =
+            DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(float),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0f,
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature),
+                    true));
+
+        public float Temperature
+        {
+            get => (float)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            float temp = (float)baseValue;
+            if (temp > 50)
+            {
+                return 50;
+            }
+            else if (temp < -50)
+            {
+                return -50;
+            }
+            else
+            {
+                return temp;
+            }
+        }
+
+        public string WindDirection
+        {
+            get; set;
+        }
+
+        public int WindSpeed
+        {
+            get; set;
+        }
+
+        public Perciptiation Perciptiation
+        {
+            get; set;
+        }
+    }
+}
